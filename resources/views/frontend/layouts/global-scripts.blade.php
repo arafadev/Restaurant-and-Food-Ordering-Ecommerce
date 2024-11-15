472c74447d1368e55296e1e708403113be7f1171
arafadev <arafa.dev@gmail.com>
2024-11-15 16:06:55 +0200

    add loader

--- resources/views/frontend/layouts/global-scripts.blade.php
+++ resources/views/frontend/layouts/global-scripts.blade.php
@@ -1,21 +1,24 @@
 <script>
-    
-    // Load product modal 
-
-
-    function loadProductModal($productId)
-    {
+    /** Loard product modal**/
+    function loadProductModal(productId) {
         $.ajax({
-            method:'GET',
-            url: '{{ route("load-product-modal", ":productId") }}'.replace(':productId', $productId),
-            success: function (response) {
-                $(".load_product_modal_body").html(response);
-                $('#cartModal').modal('show')
+            method: 'GET',
+            url: '{{ route('load-product-modal', ':productId') }}'.replace(':productId', productId),
+            beforeSend: function() {
+                // $('.overlay-container').removeClass('d-none');
+                $('.overlay').addClass('active');
             },
-            error:function(xhr, status, error){
-                alert(error)
+            success: function(response) {
+                $(".load_product_modal_body").html(response);
+                $('#cartModal').modal('show');
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            },
+            complete: function() {
+                $('.overlay').removeClass('active');
+                // $('.overlay-container').addClass('d-none');
             }
-        });
+        })
     }
-
 </script>
